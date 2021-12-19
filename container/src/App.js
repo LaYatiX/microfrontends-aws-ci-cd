34687d5c807f4cc1edf27d63387dec132c85b9d6
gpiwosz <gpiwosz@maxto.pl>
2021-12-19 20:59:11 +0100

    prefix class names for production

--- container/src/App.js
+++ container/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
-import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
-
-console.log(mount);
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 export default function App(props) {
     return (
-        <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
     )
 }
